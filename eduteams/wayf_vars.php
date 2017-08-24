28c277d2482db9ec6692f4a7fb8a350cb6721099
xchvojkajan <jan.chvojka@cesnet.cz>
2017-08-24 13:37:41 +0200

    Change branding to eduTEAMS.

--- eduteams/wayf_vars.php
+++ eduteams/wayf_vars.php
@@ -1,6 +1,6 @@
 <?php
   $prefLang = "en";
   $lang_ui = "en";
-  $organizationLabel = "Service powered by eduGAIN";
+  $organizationLabel = "Service powered by eduTEAMS";
   $organizationHelpLink = "https://www.geant.org/Innovation/eduteams";
-  $organizationHelpImage = "EduGAIN_logo.png";
+  $organizationHelpImage = "eduteams_logo28.jpg";
