c0a70b18b6252a3de0238b688ad7ba893dae0b28
Pavel Poláček <pavel.polacek@ujep.cz>
2018-06-07 11:40:49 +0200

    Added custom logos for testing purposes.

--- eduteams/wayf_vars.php
+++ eduteams/wayf_vars.php
@@ -11,3 +11,14 @@
   $filterUsersURL = "https://wiki.geant.org/pages/viewpage.action?pageId=88769546";
   $langStyle = "txt";  // dropdown or txt (select with lang names) or img (flags) or dropdown
 
+  $customLogo = "{ 
+    'https://attribute-viewer.aai.switch.ch/shibboleth': { 'Label':'', 'Link':'https://www.switch.ch', 'Image':'custom/switch-logo.png', 'ImageAlt':'Switch logo' },
+    'https://attribute-viewer.aai.switch.ch/interfederation-test/shibboleth': { 'Label':'', 'Link':'https://www.switch.ch', 'Image':'custom/switch-logo.png', 'ImageAlt':'Switch logo' },
+
+    'https://welcome.lifescienceid.org/metadata/backend.xml': { 'Label':'', 'Link':'https://aarc-project.eu/', 'Image':'custom/aarc-logo.png', 'ImageAlt':'AARC logo' },
+    'https://welcome.pilot.lifescienceid.org/metadata/backend.xml': { 'Label':'', 'Link':'https://aarc-project.eu/', 'Image':'custom/aarc-logo.png', 'ImageAlt':'AARC logo' },
+
+    'https://repository.jisc.ac.uk/shibboleth': { 'Label':'', 'Link':'https://www.jisc.ac.uk', 'Image':'custom/jisc-logo.png', 'ImageAlt':'JISC logo' },
+    'https://www.jiscmail.ac.uk/shibboleth': { 'Label':'', 'Link':'https://www.jisc.ac.uk', 'Image':'custom/jisc-logo.png', 'ImageAlt':'JISC logo' }
+
+  }";
