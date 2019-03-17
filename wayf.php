6e3081130cafbd368bf42749340cec064a2bf667
Pavel Poláček <pavel.polacek@ujep.cz>
2019-03-17 22:48:01 +0100

    Log issues with value of return parameter.

--- wayf.php
+++ wayf.php
@@ -141,6 +141,7 @@ $checkSPDiscoveryResponseTest = false;
 if(isset($_GET['entityID'])) {
     $entityID = $_GET['entityID'];
     $checkSPDiscoveryResponseTest = checkSPDiscoveryResponse( $entityID, $returnURL );
+    $checkSPDiscoveryResponseTest = true;  // don't return error, only log it
 
     if(($useFilter && isset($jFilter['allowFeeds']) && $jFilter['allowFeeds'] !== "") || ($useFilter && isset($jFilter['allowIdPs']) && $jFilter['allowIdPs'] !== "")) {
 //    if($useFilter && isset($jFilter['allowFeeds']) && $jFilter['allowFeeds'] !== "") {
